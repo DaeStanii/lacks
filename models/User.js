088681d5e46c6666d855b262d8b27fc16e076b79
jeremysr <jeremysr@protonmail.com>
2023-09-25 20:12:26 -0600

    models now require the correct config file for sequelize

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const {Model, DataTypes} = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/config');
 const bcrypt = require('bcrypt');
 
 //much of this Jeremy copied from his homework assignment module 14
