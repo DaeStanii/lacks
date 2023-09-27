92f9fe51ef0b34b609084caa60fb94f3cf441f5c
jeremysr <jeremysr@protonmail.com>
2023-09-26 21:34:05 -0600

    added sequelize query for getting all thread data

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const {Thread, User, Comment} = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('home');
+
+    const allThreads = await Thread.findAll({
+        include:[{model:User},{model:Comment}],
+        attributes:{exclude:['password']},
+    });
+    const threads = allThreads.map((thread) => thread.get({ plain: true }));
+
+    //this is for debugging with insomnia
+    //res.status(200).json(threads);
+
+    if (req.session.loggedIn) {
+        res.render('home', threads);
+    } else {
+        res.render('home');
+    }
 });
 
 module.exports = router;
