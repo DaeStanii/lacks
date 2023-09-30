d40ded9e739551ff9c3577abbb3968a45b97f16e
jeremysr <jeremysr@protonmail.com>
2023-09-30 10:40:36 -0600

    fixed api routes and added signup route/login route to their own folders

---
+++ controllers/api/signup.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {User} = require('../../models');
+
+// Create new user
+router.post('/signup', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 //const apiRoutes = require('./api');
 const homeRoutes = require('./home');
-const loginRoutes = require('./api/login');
+const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
 //router.use('/api', apiRoutes);
-router.use('/login', loginRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
