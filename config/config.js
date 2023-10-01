fa132656c5f16ad7845ef1feed0ef22209d9388e
jeremysr <jeremysr@protonmail.com>
2023-10-01 17:11:51 -0600

    added some railway.app variables for SQL

--- config/config.js
+++ config/config.js
@@ -6,13 +6,27 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 
+// } else {
+//     sequelize = new Sequelize(
+//         process.env.DB_NAME,
+//         process.env.DB_USER,
+//         process.env.DB_PASSWORD,
+//         {
+//             host: 'localhost',
+//             dialect: 'mysql',
+//         }
+//     );
+// }
+
 } else {
     sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
+        process.env.MYSQL_URL,
+        process.env.MYSQLDATABASE,
+        process.env.MYSQLUSER,
+        process.env.MYSQLPASSWORD,
+        process.env.MYSQLPORT,
         {
-            host: 'localhost',
+            host: process.env.MYSQLHOST,
             dialect: 'mysql',
         }
     );
