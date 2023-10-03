9c4ddc9326ba44a53dcd69f367152ea6e9929a2c
jeremysr <jeremysr@protonmail.com>
2023-10-02 22:52:10 -0600

    edited logout route

--- controllers/api/logout.js
+++ controllers/api/logout.js
@@ -3,13 +3,11 @@ const router = require('express').Router();
 // Logout
 // Route Works!!
 router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).redirect('/');
-        });
-    } else {
-        res.status(404).end();
-    }
+    
+    req.session.destroy(() => {
+        res.status(204).redirect('/');
+    });
+    
 });
 
 module.exports = router;
